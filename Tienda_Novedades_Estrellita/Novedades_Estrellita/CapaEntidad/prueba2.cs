14d3e9ac64714eb72e917eabd99965857415847c
marchan07 <neil_andrademarchan@hotmail.com>
2024-05-30 10:22:35 -0600

    se creo el archivo prueba2.cs en remoto

--- Tienda_Novedades_Estrellita/Novedades_Estrellita/CapaEntidad/prueba2.cs
+++ Tienda_Novedades_Estrellita/Novedades_Estrellita/CapaEntidad/prueba2.cs
@@ -2,7 +2,7 @@ public class ConsultaDatos() {
     public int Id_Alumno {get; set;}
     public string Nombre {get; set;}
     public string Carrera {get; set;}
-
+ 
     public ConsultaDatos(){
     }
 }
