04578b2dfcc84c06f2ed40571f9d677dd13aca71
marchan07 <neil_andrademarchan@hotmail.com>
2024-05-30 10:19:46 -0600

    Create prueba2.cs
    se creo el archivo prueba2.cs en remoto

---
+++ Tienda_Novedades_Estrellita/Novedades_Estrellita/CapaEntidad/prueba2.cs
@@ -0,0 +1,8 @@
+public class ConsultaDatos() {
+    public int Id_Alumno {get; set;}
+    public string Nombre {get; set;}
+    public string Carrera {get; set;}
+
+    public ConsultaDatos(){
+    }
+}
